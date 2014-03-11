7dac2035261d2d0b9e3ab4c8d6a5adf7ba41c63a
eightyfour <handroid69@googlemail.com>
2014-03-11 17:58:49 +0100

    Add ready method in canny

--- canny.js
+++ canny.js
@@ -10,44 +10,58 @@ var canny = (function () {
 
     console.log('INITIALIZE CANNY MODULE');
 
-    var parseNode = function (node, name, cb) {
-        var that = this, gdModuleChildren = [].slice.call(node.querySelectorAll('[' + name + '-mod]'));
+    var readyQueue = [],
+        callReadQueue = function () {
+            (function reduce() {
+                var fc = readyQueue.pop();
+                if (fc) {
+                    fc();
+                    reduce();
+                } else {
+                    readyQueue = null;
+                }
+            }());
+        },
+        parseNode = function (node, name, cb) {
+            var that = this, gdModuleChildren = [].slice.call(node.querySelectorAll('[' + name + '-mod]'));
 
-        gdModuleChildren.forEach(function (node) {
-            var attribute = node.getAttribute(name + '-mod'), attr, viewPart, attributes, cannyVar;
+            gdModuleChildren.forEach(function (node) {
+                var attribute = node.getAttribute(name + '-mod'), attr, viewPart, attributes, cannyVar;
 
-            attributes = attribute.split(' ');
+                attributes = attribute.split(' ');
 
-            attributes.forEach(function (eachAttr) {
-                if (that.hasOwnProperty(eachAttr)) {
-                    if (node.getAttribute(name + '-mod')) {
-                        cannyVar = node.getAttribute(name + '-var');
-                        if (cannyVar) {
-                            attr = cannyVar.split("\'").join('\"');
-                            if (/:/.test(attr)) {
-                                // could be a JSON
-                                viewPart = JSON.parse(attr);
-                            } else {
-                                viewPart = attr;
+                attributes.forEach(function (eachAttr) {
+                    if (that.hasOwnProperty(eachAttr)) {
+                        if (node.getAttribute(name + '-mod')) {
+                            cannyVar = node.getAttribute(name + '-var');
+                            if (cannyVar) {
+                                attr = cannyVar.split("\'").join('\"');
+                                if (/:/.test(attr)) {
+                                    // could be a JSON
+                                    viewPart = JSON.parse(attr);
+                                } else {
+                                    viewPart = attr;
+                                }
                             }
                         }
+                        that[eachAttr].add(node, viewPart);
                     }
-                    that[eachAttr].add(node, viewPart);
-                }
+                });
             });
-        });
-        cb && cb();
-    };
+            cb && cb();
+        };
 
     domready(function () {
         parseNode.apply(canny, [document, 'canny']);
         // find solution for calling ready...
         Object.keys(canny).forEach(function (module) {
-//  todo save all new registered objects and call there after appending all.
+        //  todo save all new registered objects and call there after appending all.
             if (canny[module].hasOwnProperty('ready')) {
                 canny[module].ready();
             }
         });
+        // call canny reader queue
+        callReadQueue();
     });
 
     return {
@@ -56,6 +70,13 @@ var canny = (function () {
                 this[name] = module;
             } else {
                 console.error('canny: Try to register module with name ' + name + ' twice');
+            }
+        },
+        ready : function (fc) {
+            if (readyQueue !== null) {
+                readyQueue.push(fc);
+            } else {
+                fc();
             }
         },
         cannyParse : function (node, name, cb) {
