fc5d097e67c6a9ee75e0ca72b11e4836313a9816
han <han@thinkuseful.de>
2014-04-09 16:22:58 +0200

    made canny mods can be used without require

--- mod/async.js
+++ mod/async.js
@@ -1,44 +1,53 @@
-/*global */
+/*global canny */
 /*jslint browser: true*/
 
 /**
- * E.g.: gd-module="flowControl" gd-attr="{'view' : 'viewToShow'}"
+ * E.g.:
+ * canny.async.load(URL, function (src) {
+ *     node.innerHTML = src;
+ *     // trigger canny parse to register canny on our new modules
+ *     canny.cannyParse(node, function () {
+ *         console.log('CANNY PARSE DONE');
+ *     });
+ * });
  *
  */
-var async = (function () {
+(function () {
     "use strict";
-
-    var fc = {
-            loadHtml : function (c) {
-                var r = new XMLHttpRequest();
-                r.open(c.method, c.path, true);
-                r.onreadystatechange = function () {
-                    if (r.readyState != 4 || r.status != 200) {
-                        c.cb(false);
-                    } else {
-                        console.log('Success: ' + r.responseText);
-                        c.cb(r.responseText);
+    var async = (function () {
+            var fc = {
+                    loadHtml : function (c) {
+                        var r = new XMLHttpRequest();
+                        r.open(c.method, c.path, true);
+                        r.onreadystatechange = function () {
+                            if (r.readyState != 4 || r.status != 200) {
+                                c.cb(false);
+                            } else {
+                                console.log('Success: ' + r.responseText);
+                                c.cb(r.responseText);
+                            }
+                        };
+                        r.send(c.param);
+                    }
+                },
+                modViews = {
+                    ready : function () {console.log('async is ready'); },
+                    add : function (node, attr) {    // part of api
+                        // TODO implement logic for loading it directly from html
+                    },
+                    load : function (path, cb) {
+                        fc.loadHtml({
+                            method: 'GET',
+                            path: path,
+                            param: '',
+                            cb: cb || function () {}
+                        });
                     }
                 };
-                r.send(c.param);
-            }
-        },
-        modViews = {
-            ready : function () {console.log('async is ready'); },
-            add : function (node, attr) {    // part of api
-                // TODO implement logic for loading it directly from html
-            },
-            load : function (path, cb) {
-                fc.loadHtml({
-                    method: 'GET',
-                    path: path,
-                    param: '',
-                    cb: cb || function () {}
-                });
-            }
-        };
 
-    return modViews;
+            return modViews;
+        }());
+    // export as module or bind to global
+    if (typeof module !== 'undefined') { module.exports = async; } else {canny.add('async', async); }
+
 }());
-
-module.exports = async;
