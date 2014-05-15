a190fea74a58807ccb34406f179fbf03191ba5c8
han <han@thinkuseful.de>
2014-05-16 00:14:39 +0200

    add latest

---
+++ samples/flowControlTriggerSameView.js
@@ -0,0 +1,32 @@
+
+canny.add('flowControlTriggerSameView', (function () {
+    "use strict";
+    function CheckBox(node) {
+        this.node = node;
+    }
+    var checkbox = {};
+    return {
+            add : function (elem, attr) {
+            if (typeof attr === 'object' && attr.hasOwnProperty('checkbox')) {
+                elem.setAttribute('value', attr['checkbox']);
+                elem.innerHTML = attr['checkbox'];
+                checkbox[attr['checkbox']] = new CheckBox(elem);
+            } else {
+                elem.addEventListener('click', function () {
+                    var boxes = Object.keys(checkbox),
+                        ar = [attr];
+                    boxes.forEach(function (checkBox) {
+                        var value;
+                        if (checkbox[checkBox].node.checked) {
+                            ar.push(checkbox[checkBox].node.getAttribute('value'));
+                        }
+                    });
+                    canny.flowControl.show.apply(null, ar);
+                });
+            }
+        },
+        ready : function () {
+            console.log('flowControlTriggerSameView ready!');
+        }
+    };
+}()));
