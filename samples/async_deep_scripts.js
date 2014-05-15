5656004dd8f32dcf7afede8569ee81f423e19e18
han <han@thinkuseful.de>
2014-05-15 22:15:40 +0200

    add multiple flowControl example

--- samples/async_deep_scripts.js
+++ samples/async_deep_scripts.js
@@ -1,10 +1,10 @@
 canny.add('async_deep_scripts', (function () {
     "use strict";
-    var node    ;
+    var node;
     return {
         add : function (elem, attr) {
             node = elem;
         },
         ready : function () {}
-    }
-}()))
+    };
+}()));
