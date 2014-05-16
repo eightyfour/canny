4f43edb10fa41b1391b43f7e69070a6962ef8a50
han <han@thinkuseful.de>
2014-05-16 12:13:18 +0200

    add require sample

--- samples/requireSample.js
+++ samples/requireSample.js
@@ -1,25 +1,14 @@
 (function () {
     "use strict";
-    canny.add('whiskerSample', (function () {
-        var node,
-            data1 = {
-                name : 'whisker',
-                text : 'Dynamical included text'
-            },
-            data2 = function () {
-                var n = Math.round(Math.random(0, 10) * 10);
-                return {
-                    name : 'whisker',
-                    text : 'rand: ' + n
-                };
-            }
+    canny.add('requireSample', (function () {
+
         return {
-            data1 : data1,
-            data2 : function () {
-                return data2();
+            add : function (elem, attr) {
+                console.log('ADD requireSample');
+                elem.style.backgroundColor = attr;
             },
-            add : function (elem) {
-                node = elem;
+            ready : function () {
+                console.log('requireSample READY');
             }
         };
     }()));
