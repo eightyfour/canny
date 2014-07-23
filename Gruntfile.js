85a9ad61b78cfb14451e739d84c7ed9f5380a15f
herrmannplatz <rene@herrmannplatz.de>
2014-07-23 21:26:04 +0200

    basic boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+
+module.exports = function( grunt ) {
+
+grunt.initConfig({
+    pkg: grunt.file.readJSON( "package.json" ),
+
+    qunit: {
+        all: ['test/**/*.html']
+    }
+});
+
+grunt.loadNpmTasks('grunt-contrib-qunit');
+
+grunt.registerTask( "default", [ "qunit" ]);
+
+};
