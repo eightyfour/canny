6c5abc06ace19970aa8133013e05f4763b1a3c00
eightyfour <handroid69@googlemail.com>
2014-07-24 23:48:57 +0200

    fix issues that canny runs still with browserify and grunt

--- mod/async.js
+++ mod/async.js
@@ -196,8 +196,8 @@
         return modViews;
     }());
     // export as module or bind to global
-    if (typeof exports !== 'undefined') {
-        exports = async;
+    if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+        module.exports = async;
     } else {
         canny.add('async', async);
     }
