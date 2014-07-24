fd94091bfe8fcbdbc8be6af1e8d692115af6a310
herrmannplatz <rene@herrmannplatz.de>
2014-07-24 11:15:07 +0200

    added async test files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,33 @@
 module.exports = function( grunt ) {
 
 grunt.initConfig({
-    pkg: grunt.file.readJSON( "package.json" ),
+
+    pkg: grunt.file.readJSON( 'package.json' ),
+
+    connect: {
+        server: {
+            options: {
+                port: 8000,
+                base: '.'
+            }
+        }
+    },
 
     qunit: {
-        all: ['test/**/*.html']
+        all: {
+            options: {
+                urls: [
+                    'http://localhost:8000/test/canny-core.html',
+                    'http://localhost:8000/test/canny-async.html'
+                ]
+            }
+        }
     }
 });
 
 grunt.loadNpmTasks('grunt-contrib-qunit');
+grunt.loadNpmTasks('grunt-contrib-connect');
 
-grunt.registerTask( "default", [ "qunit" ]);
+grunt.registerTask( 'default', ['connect', 'qunit']);
 
 };
--- test/unit/canny-core.js
+++ test/unit/canny-core.js
@@ -1,12 +1,9 @@
-// Let's test this function
-function isEven(val) {
-    return val % 2 === 0;
-}
+
  
-test('isEven()', function() {
-    ok(isEven(0), 'Zero is an even number');
-    ok(isEven(2), 'So is two');
-    ok(isEven(-4), 'So is negative four');
-    ok(!isEven(1), 'One is not an even number');
-    ok(!isEven(-7), 'Neither is negative seven');
-})
+test('requirements', function() {
+    ok(canny, 'canny is available');
+});
+
+// test( "canny add", function( assert ) {
+//     ok(true, 'canny add');
+// });
