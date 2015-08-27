4bb61f0307922ecd92fa876075bf69d80b0f6e3e
eightyfour <eightyfour@thinkuseful.de>
2015-08-27 23:38:54 +0200

    add code coverage with karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -35,14 +35,24 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'mod/*.js': ['coverage'],
+            'canny.js': ['coverage']
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress','coverage'],
 
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        },
 
         // web server port
         port: 9876,
