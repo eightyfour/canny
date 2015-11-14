c4d97fa3260ff0517a1ad688b5405c2c349c9ea6
eightyfour <eightyfour@thinkuseful.de>
2015-11-14 23:49:48 +0100

    add first working beardie

--- mod/beardie.js
+++ mod/beardie.js
@@ -63,34 +63,50 @@
              *
              * @param node
              * @param dataObj
-             * @param attr
+             * @param itemName
              */
-            function compileTextNode(node, dataObj, attr) {
-                var tokens = parse(node.nodeValue), obj = dataObj, el, token, i, l;
+            function compileTextNode(node, dataObj, itemName) {
+                var tokens = parse(node.nodeValue),
+                    obj = dataObj,
+                    el, token, i, l, tmp, tokenObjectProperty, val;
 
                 if (!tokens || obj === undefined || typeof obj === 'string') {return; }
 
                 for (i = 0, l = tokens.length; i < l; i++) {
                     token = tokens[i];
 
-                    if (token.key && obj.hasOwnProperty(token.key)) { // a binding
-                        el = document.createTextNode(obj[token.key]);
-                        if (whiskerUpdateMap.hasOwnProperty(attr)) {
-                            if (whiskerUpdateMap[attr].keyMap === undefined) {
-                                whiskerUpdateMap[attr].keyMap = {};
+                    if (typeof token === 'object') {
+                        tmp = token.key.split('.');
+                        if (tmp.length > 0 && tmp[0] === itemName) {
+                            tokenObjectProperty = tmp.slice(1).join('.');
+                            if (typeof obj === 'object') {
+                                val = getGlobalCall(tokenObjectProperty, obj);
+                            } else {
+                                val = obj;
                             }
-                            if (!whiskerUpdateMap[attr].keyMap[token.key]) {
-                                whiskerUpdateMap[attr].keyMap[token.key] = [];
-                            }
-                            whiskerUpdateMap[attr].keyMap[token.key].push(el);
+                        } else {
+                            // just a string?
+                            val = obj;
                         }
-                        node.parentNode.insertBefore(el, node);
-                    } else { // a plain string
+                        if (typeof val === 'string' || typeof val === 'number') {
+                            el = document.createTextNode(val);
+                            node.parentNode.insertBefore(el, node);
+                        } else if (typeof val === 'boolean') {
+                            el = document.createTextNode(val.toString());
+                            node.parentNode.insertBefore(el, node);
+                        } else if (typeof val === 'function') {
+                            el = document.createTextNode(val(node.parentNode));
+                            node.parentNode.insertBefore(el, node);
+                        } else {
+                            // restore the token... looks like is not mine
+                            el = document.createTextNode('{{' + token.key + '}}');
+                            node.parentNode.insertBefore(el, node);
+                        }
+                    } else {
                         el = document.createTextNode(token);
+                        // just normal string put back to view
                         node.parentNode.insertBefore(el, node);
                     }
-                    // insert node
-
                 }
                 node.parentNode.removeChild(node);
             }
@@ -98,63 +114,156 @@
              *
              * @param node
              * @param dataObj
-             * @param attr
+             * @param itemName
              */
-            function compileElement(node, dataObj, attr) {
+            function compileElement (node, dataObj, itemName) {
                 // recursively compile childNodes
                 if (node.hasChildNodes()) {
                     [].slice.call(node.childNodes).forEach(function (child) {
-                        compile(child, dataObj, attr);
+                        compile(child, dataObj, itemName);
                     });
                 }
             }
             /**
              *  Compile a DOM node (recursive)
+             * @param node
+             * @param dataObj
+             * @param itemName
+             * @returns {*}
              */
-            function compile(node, dataObj, attr) {
+            function compile(node, dataObj, itemName) {
                 var nodeType = node.nodeType;
                 if (nodeType === 1 && node.tagName !== 'SCRIPT') { // a normal node
-                    compileElement(node, dataObj, attr);
+                    compileElement(node, dataObj, itemName);
                 } else if (nodeType === 3) {
-                    compileTextNode(node, dataObj, attr);
+                    compileTextNode(node, dataObj, itemName);
                 }
-            }
-            /**
-             *
-             * @param node
-             * @param attr
-             */
-            function initialize(node, attr) {
-                var dataObj = getGlobalCall(attr, window), obj;
-                if (typeof dataObj === 'function') {
-                    obj = dataObj();
-                } else {
-                    obj = dataObj;
-                }
-                if (obj.hasOwnProperty('whiskerUpdate')) {
-                    if (!whiskerUpdateMap[attr]) {
-                        whiskerUpdateMap[attr] = {
-                            obj : obj,
-                            keyMap : undefined
-                        };
-                    }
-                    whiskerUpdateMap[attr].obj.whiskerUpdate(function (data) {
-                        if (!whiskerUpdateMap[attr].keyMap) {
-                            initialize(node, attr);
-                        }
 
-                        if (whiskerUpdateMap[attr].keyMap) {
-                            Object.keys(whiskerUpdateMap[attr].keyMap).forEach(function (whiskerName) {
-                                if (data[whiskerName]) {
-                                    whiskerUpdateMap[attr].keyMap[whiskerName].forEach(function (node) {
-                                        node.nodeValue = data[whiskerName];
-                                    });
-                                }
-                            });
+                return node;
+            }
+
+            /**
+             * register click events
+             *
+             * @param clone
+             * @param item
+             * @param itemName
+             */
+            function handleEvents(clone, obj, itemName) {
+                var onClick = 'on-click';
+                // check children of clone
+                [].slice.call(clone.querySelectorAll('[' + onClick + ']')).forEach(function (node) {
+                    getLoopValueFromAttribute(node, obj, itemName, onClick, function (val) {
+                        if (typeof val === 'function') {
+                            node.addEventListener('click', val);
+                        } else {
+                            console.log('beardie:can not register click listener without a function', node);
                         }
                     });
+                });
+            }
+
+            /**
+             * Replaces expressions for all tag attributes
+             *
+             * @param clone
+             * @param obj
+             * @param itemName (currently not in used but needs to be checked)
+             */
+            function handleAttributes(clone, obj, itemName) {
+                (function searchForExpressions(children) {
+                    [].slice.call(children).forEach(function (node) {
+                        var i, attr;
+                        if (node.children.length > 0) {
+                            // do it recursive for all children
+                            searchForExpressions(node.children);
+                        }
+                        for (i = 0; i < node.attributes.length; i++) {
+                            attr = node.attributes[i];
+                            if (/\{\{/.test(attr.textContent)) {
+                                if (attr.name) {
+                                    (function () {
+                                        var token = parse(attr.textContent),
+                                            endData = [], tmpToken, j, globalObj, tmpTokenSplit;
+                                        for (j = 0; j < token.length; j++) {
+                                            tmpToken = token[j];
+                                            if (typeof tmpToken === 'object') {
+                                                if (/\./.test(tmpToken.key)) {
+                                                    tmpTokenSplit = tmpToken.key.split('.').slice(1).join('.');
+                                                } else {
+                                                    tmpTokenSplit = tmpToken.key;
+                                                }
+                                                if (typeof obj === 'object') {
+                                                    globalObj = getGlobalCall(tmpTokenSplit, obj);
+                                                    if (typeof globalObj === 'function') {
+                                                        endData.push(globalObj());
+                                                    } else {
+                                                        endData.push(globalObj);
+                                                    }
+                                                } else if (typeof obj === 'string') {
+                                                    endData.push(obj);
+                                                } else if (typeof obj === 'function') {
+                                                    endData.push(obj(node));
+                                                }
+
+                                            } else {
+                                                endData.push(tmpToken.trim());
+                                            }
+                                        }
+                                        attr.textContent = endData.join(' ');
+                                    }())
+                                }
+                            }
+                        }
+                    });
+                }(clone.children));
+            }
+
+            /**
+             * Looped through the collection and do the logic for each clone instance.
+             * Actually it supports only collection - no objects.
+             * @param node
+             * @param scopeName
+             * @param collection
+             * @param template
+             */
+            function registerTemplate(node, scopeName, data) {
+
+                if (typeof data === 'object') {
+                    handleEvents(node, data, scopeName);
+                    handleAttributes(node, data, scopeName);
+                    // replace texts:
+                    compile(node, data, scopeName);
+                } else {
+                    console.error('beardie:registerTemplate detect none acceptable data argument', data);
                 }
-                compile(node, obj, attr);
+            }
+
+            /**
+             * TODO description
+             * Create a new beardie instance and do the "magic".
+             * @param node
+             * @param scopeName
+             * @param data
+             */
+            function exec(node, data, scopeName) {
+                var currentScope = scopeName;
+                if (typeof data === 'function') {
+                    data(function (scope, data) {
+                        if (data !== undefined) {
+                            currentScope  = scope;
+                        } else {
+                            data = scope;
+                        }
+                        // better would be a update children but this is much effort to detect
+//                        [].slice.call(node.children).forEach(function (child) {
+//                            node.removeChild(child);
+//                        });
+                        registerTemplate(node, currentScope, data);
+                    });
+                } else {
+                    registerTemplate(node, currentScope, data)
+                }
             }
 
             return {
@@ -162,8 +271,22 @@
                     return whiskerUpdateMap;
                 },
                 add : function (node, attr) {
-                    if (typeof attr === 'string') {
-                        initialize(node, attr);
+                    var inPointer;
+                    if (typeof attr === 'object' && attr.in && attr.bind) {
+                        if (typeof attr.in === 'string') {
+                            // TODO replace window with this and also other instances could use the magic as closure
+                            inPointer = getGlobalCall(attr.in, window);
+                        } else {
+                            inPointer = attr.in;
+                        }
+                        exec(node, inPointer, attr.bind);
+                    } else {
+                        inPointer = getGlobalCall(attr, window);
+                        if (typeof inPointer === 'function') {
+                            exec(node, inPointer);
+                        } else {
+                            console.warn('beardie:add none acceptable attributes', attr);
+                        }
                     }
                 }
             };
@@ -201,7 +324,7 @@
     }
 
     // export as module or bind to global
-    if (typeof module !== 'undefined') {
+    if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
         module.exports = beardie;
     } else {
         canny.add('beardie', beardie);
--- spec/beardieSpec.js
+++ spec/beardieSpec.js
@@ -9,11 +9,13 @@ canny.add('beardieSample', (function () {
                 name : 'beardie'
             };
         },
-        data3 = {
-            user : {
-                name: 'Peter',
-                age: 30
-            }
+        data3 = function (cb) {
+            cb({
+                user : {
+                    name: 'Peter',
+                    age: 30
+                }
+            });
         },
         inputs = (function () {
             var update = [];
@@ -29,21 +31,26 @@ canny.add('beardieSample', (function () {
                 }
             };
         }()),
-        triggerWhiskerUpdate = (function () {
-            var whiskerUpdate = function () {},
+        triggerUpdate = (function () {
+            var update = function () {},
                 data = data2();
+
             return {
                 // trigger this to update the data
                 updateData : function () {
-                    whiskerUpdate(data2());
+                    update('data', {
+                        name : 'foo2',
+                        text : 'bar2'
+                    });
                 },
-                // part of api to whisker
-                whiskerUpdate : function (cb) {
-                    whiskerUpdate = cb;
-                },
-                // object
-                name : data.name,
-                text : data.text
+                // part of api to beardie
+                data : function (fc) {
+                    update = fc;
+                    fc('data', {
+                        name : 'foo1',
+                        text : 'bar1'
+                    });
+                }
             };
         }());
     return {
@@ -54,12 +61,18 @@ canny.add('beardieSample', (function () {
         /**
          * get the object from a function
          */
-        data2 : function () {
-            return data2();
+        data2 : function (fc) {
+            fc(data2());
         },
         inputs : inputs,
         data3 : data3,
-        triggerWhiskerUpdate : triggerWhiskerUpdate,
+        data4 : function (fc) {
+            fc('scope2', {
+                name : 'birdy',
+                age : 0
+            })
+        } ,
+        triggerUpdate : triggerUpdate,
         add : function (node, attr) {}
     };
 }()));
@@ -76,20 +89,44 @@ describe('Check beardie', function() {
         expect(canny.beardie).toBeDefined();
     });
 
-    it('should include dynamical texts', function () {
-        var data = mainNode.querySelector('#beardie_data1');
-        expect(data.innerHTML).toEqual("Hello I'm beardie and I include some texts dynamical: Dynamical included text.");
+    describe('loadFromObject', function () {
+        var node;
+
+        beforeAll(function () {
+            node = mainNode.querySelector('#loadFromObject');
+        });
+
+        it('should add the normal text to each strong', function () {
+            var data = node.children[0].children;
+            expect(data[0].innerHTML).toEqual("Peter");
+            expect(data[1].innerHTML).toEqual("30");
+        });
+
+        it('should not remove the other expression', function () {
+            var data = node.children[1];
+            expect(data.innerHTML).toEqual("And this expression should stay: {{scopeX.foo}}.");
+        });
+
+        it('should have replaced the inner expression from a other scope correctly', function () {
+            var data = node.children[2].children[0];
+            expect(data.innerHTML).toEqual("Hello I'm birdy and I'm 0 years old.");
+        });
     });
 
-    it('should load the data from a function', function () {
-        var data = mainNode.querySelector('#beardie_data_function').children;
-        expect(data[0].innerHTML).toEqual("beardie");
-        expect(data[1].innerHTML).toEqual("beardie");
-        expect(data[2].innerHTML).toEqual("beardie");
-        expect(data[3].innerHTML).toEqual("beardie");
-    });
-
-    it('should update the values correctly', function () {
-        // TODO
-    });
+//
+//    it('should load the data from a inline object', function () {
+//        var data = mainNode.querySelector('#ẗestInnerJSON').children;
+//        expect(data[0].innerHTML).toEqual("My name is Carlo");
+//        expect(data[1].innerHTML).toEqual("And here are my message: bar");
+//    });
+//
+//    it('should update the values correctly', function () {
+//        // TODO
+//    });
+//
+//    it('should not replace unkown placeholder', function () {
+//        var data = mainNode.querySelector('#testExpressionStayIfNotExists').children;
+//        expect(data[0].innerHTML).toEqual("My name is Carlo");
+//        expect(data[1].innerHTML).toEqual("And this should stay {{message}}");
+//    });
 });
